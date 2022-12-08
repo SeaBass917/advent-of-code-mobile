59af67e6aa42c7f1ed34a5e08f70d07cabea51d9
SeaBass917 <sthiem@email.arizona.edu>
2022-12-08 09:36:25 -0500

    +1

---
+++ lib/web_view_better.dart
@@ -0,0 +1,47 @@
+import 'package:advent_of_code/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+class WebViewBetter extends StatefulWidget {
+  const WebViewBetter(this.url, this.cookieManager, {super.key});
+  final Uri url;
+  final CookieManager cookieManager;
+
+  @override
+  State<WebViewBetter> createState() => _WebViewBetter();
+}
+
+class _WebViewBetter extends State<WebViewBetter> {
+  late InAppWebViewController _webViewController;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('InAppWebView Example'),
+        ),
+        body: InAppWebView(
+          initialUrlRequest: URLRequest(url: widget.url),
+          initialOptions: InAppWebViewGroupOptions(
+            crossPlatform: InAppWebViewOptions(),
+          ),
+          onWebViewCreated: (InAppWebViewController controller) {
+            _webViewController = controller;
+          },
+          onTitleChanged: (controller, title) {
+            /// TODO: This is hacky. PLease be smarter about deciding when we are done. e.g. is the cookie here?
+            if (title == "Advent of Code 2022") {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const MyHomePage(),
+                  ));
+            }
+          },
+          onLoadStop: (controller, url) async {},
+        ),
+      ),
+    );
+  }
+}
